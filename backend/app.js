25aaf3cfdd28a27e369eb727bc77a89052d46f7c
bariskocer <99131317+bariskocer@users.noreply.github.com>
2025-05-29 22:14:46 +0300

    fix img

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,29 @@
-import fs from 'node:fs/promises';
+import fs from "node:fs/promises";
 
-import bodyParser from 'body-parser';
-import express from 'express';
+import bodyParser from "body-parser";
+import express from "express";
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, DELETE, OPTIONS'
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
   );
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-Requested-With,content-type'
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
   );
   next();
 });
 
-app.get('/events', async (req, res) => {
+app.get("/events", async (req, res) => {
   const { max, search } = req.query;
-  const eventsFileContent = await fs.readFile('./data/events.json');
+  const eventsFileContent = await fs.readFile("./data/events.json");
   let events = JSON.parse(eventsFileContent);
 
   if (search) {
@@ -48,17 +48,21 @@ app.get('/events', async (req, res) => {
   });
 });
 
-app.get('/events/images', async (req, res) => {
-  const imagesFileContent = await fs.readFile('./data/images.json');
-  const images = JSON.parse(imagesFileContent);
+app.get("/events/images", async (req, res) => {
+  const imagesFileContent = await fs.readFile("./data/images.json", "utf-8");
+  const filenames = JSON.parse(imagesFileContent).images;
 
-  res.json({ images });
+  const origin = `${req.protocol}://${req.get("host")}`;
+
+  const urls = filenames.map((name) => `${origin}/${name}`);
+
+  res.json({ images: urls });
 });
 
-app.get('/events/:id', async (req, res) => {
+app.get("/events/:id", async (req, res) => {
   const { id } = req.params;
 
-  const eventsFileContent = await fs.readFile('./data/events.json');
+  const eventsFileContent = await fs.readFile("./data/events.json");
   const events = JSON.parse(eventsFileContent);
 
   const event = events.find((event) => event.id === id);
@@ -74,11 +78,11 @@ app.get('/events/:id', async (req, res) => {
   }, 1000);
 });
 
-app.post('/events', async (req, res) => {
+app.post("/events", async (req, res) => {
   const { event } = req.body;
 
   if (!event) {
-    return res.status(400).json({ message: 'Event is required' });
+    return res.status(400).json({ message: "Event is required" });
   }
 
   console.log(event);
@@ -91,10 +95,10 @@ app.post('/events', async (req, res) => {
     !event.image?.trim() ||
     !event.location?.trim()
   ) {
-    return res.status(400).json({ message: 'Invalid data provided.' });
+    return res.status(400).json({ message: "Invalid data provided." });
   }
 
-  const eventsFileContent = await fs.readFile('./data/events.json');
+  const eventsFileContent = await fs.readFile("./data/events.json");
   const events = JSON.parse(eventsFileContent);
 
   const newEvent = {
@@ -104,17 +108,17 @@ app.post('/events', async (req, res) => {
 
   events.push(newEvent);
 
-  await fs.writeFile('./data/events.json', JSON.stringify(events));
+  await fs.writeFile("./data/events.json", JSON.stringify(events));
 
   res.json({ event: newEvent });
 });
 
-app.put('/events/:id', async (req, res) => {
+app.put("/events/:id", async (req, res) => {
   const { id } = req.params;
   const { event } = req.body;
 
   if (!event) {
-    return res.status(400).json({ message: 'Event is required' });
+    return res.status(400).json({ message: "Event is required" });
   }
 
   if (
@@ -125,16 +129,16 @@ app.put('/events/:id', async (req, res) => {
     !event.image?.trim() ||
     !event.location?.trim()
   ) {
-    return res.status(400).json({ message: 'Invalid data provided.' });
+    return res.status(400).json({ message: "Invalid data provided." });
   }
 
-  const eventsFileContent = await fs.readFile('./data/events.json');
+  const eventsFileContent = await fs.readFile("./data/events.json");
   const events = JSON.parse(eventsFileContent);
 
   const eventIndex = events.findIndex((event) => event.id === id);
 
   if (eventIndex === -1) {
-    return res.status(404).json({ message: 'Event not found' });
+    return res.status(404).json({ message: "Event not found" });
   }
 
   events[eventIndex] = {
@@ -142,31 +146,31 @@ app.put('/events/:id', async (req, res) => {
     ...event,
   };
 
-  await fs.writeFile('./data/events.json', JSON.stringify(events));
+  await fs.writeFile("./data/events.json", JSON.stringify(events));
 
   setTimeout(() => {
     res.json({ event: events[eventIndex] });
   }, 1000);
 });
 
-app.delete('/events/:id', async (req, res) => {
+app.delete("/events/:id", async (req, res) => {
   const { id } = req.params;
 
-  const eventsFileContent = await fs.readFile('./data/events.json');
+  const eventsFileContent = await fs.readFile("./data/events.json");
   const events = JSON.parse(eventsFileContent);
 
   const eventIndex = events.findIndex((event) => event.id === id);
 
   if (eventIndex === -1) {
-    return res.status(404).json({ message: 'Event not found' });
+    return res.status(404).json({ message: "Event not found" });
   }
 
   events.splice(eventIndex, 1);
 
-  await fs.writeFile('./data/events.json', JSON.stringify(events));
+  await fs.writeFile("./data/events.json", JSON.stringify(events));
 
   setTimeout(() => {
-    res.json({ message: 'Event deleted' });
+    res.json({ message: "Event deleted" });
   }, 1000);
 });
 
